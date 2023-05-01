7365dfe0e7e3cd342755d89a95c3aee0d715a4a5
YX536 <71821326+YX536@users.noreply.github.com>
2023-05-01 12:00:49 +0800

    Update Main.py

--- Optimized/Main.py
+++ Optimized/Main.py
@@ -10,14 +10,13 @@ import Gabor
 import Features
 a=time.time()
 detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
-# Video_path = r'D:/FeatureExtractionAuto/H/*.mpg'
-VideoPath ='D:/FirstFrame/bbae1a.mpg'
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat") # path of "shape_predictor_68_face_landmarks.dat"
+VideoPath ='D:/FirstFrame/bbae1a.mpg'  # video path
 Frame = 'D:/FE/Picture/'# path to store pictures
-MouthPath = 'D:/FE/mouth/'  # path to store mouth
-GaborPath = 'D:/FE/Gabor/'#path to store Gabor features
-SheetPath = 'D:/FE/Sheet/' # path to storSheetPath
-FeaturesPath = 'D:/FE/Features/'  # path to store sheets
+MouthPath = 'D:/FE/mouth/'  # path to store mouth region
+GaborPath = 'D:/FE/Gabor/'# path to store Gabor features
+SheetPath = 'D:/FE/Sheet/' # path to store lip features
+FeaturesPath = 'D:/FE/Features/'  # path to store lip features
 
 
 WordVideo.Frame(detector,predictor,VideoPath,Frame,MouthPath,GaborPath,SheetPath,FeaturesPath)
